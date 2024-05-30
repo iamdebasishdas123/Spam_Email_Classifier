1d67966d22e65e207feba13616ee1f9b7b775650
Debasish Das <146256089+iamdebasishdas123@users.noreply.github.com>
2024-05-30 15:58:23 +0530

    Update scam_email.py

--- scam_email.py
+++ scam_email.py
@@ -55,7 +55,7 @@ if st.button('Predict'):
     result = model.predict(vector_input)[0]
 
     # Display result
-    if result == 0:
+    if result == 1:
         st.header('Spam')
     else:
         st.header('Not Spam')
